8913454fe653d6c1f5a0e8e37c4216d04a4074fd
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-18 17:33:48 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 2, 3, 1, 5, 7, 8, 4, 9, 6 };
+
+            int temp;
+
+            for (int j = 0; j <= arr.Length - 2; j++)
+            {
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i + 1];
+                        arr[i + 1] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sorted:");
+
+            foreach (int p in arr)
+                Console.Write(p + " ");
+
+            Console.ReadLine();
+        }
+    }
+}
